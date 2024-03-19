821f07185a72bc287552c153f8d43314b83b084b
Pera2604 <petar.savic05@gmail.com>
2024-03-19 15:52:23 +0100

    New Location Window

---
+++ TaxiMeterUI/Location.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Windows;
+
+namespace TaxiMeterUI
+{
+    public partial class Location : Window
+    {
+        public int PickupLocationLatitude { get; private set; }
+        public int PickupLocationLongitude { get; private set; }
+        public int DestinationLatitude { get; private set; }
+        public int DestinationLongitude { get; private set; }
+
+        public Location()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(tbPickupLocationLat.Text, out int first))
+            {
+                if ((first >= -90 && first <= 90))
+                {
+                    PickupLocationLatitude = first;
+                    DialogResult = true;
+                }
+                else
+                {
+                    MessageBox.Show("Invalid input. Latitude ranges from -90 to 90 degrees, please try again..", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else if (int.TryParse(tbPickupLocationLong.Text, out int second))
+            {
+                if (((second >= -180 && second <= 180)))
+                {
+                    PickupLocationLongitude = second;
+                    DialogResult = true;
+                }
+                else
+                {
+                    MessageBox.Show("Invalid input. Longitude ranges from -180 to 180 degrees, please try again..", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else if (int.TryParse(tbDestinationLat.Text, out int third))
+            {
+                if ((third >= -90 && third <= 90))
+                {
+                    DestinationLatitude = third;
+                    DialogResult = true;
+                }
+                else
+                {
+                    MessageBox.Show("Invalid input. Latitude ranges from -90 to 90 degrees, please try again..", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else if ( int.TryParse(tbDestinationLong.Text, out int fourth))
+            {
+                if ((fourth >= -180 && fourth <= 180))
+                {
+                    DestinationLongitude = fourth;
+                    DialogResult = true;
+                }
+                else
+                {
+                    MessageBox.Show("Invalid input. Longitude ranges from -180 to 180 degrees, please try again..", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid integers.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            if ( PickupLocationLatitude != null && PickupLocationLongitude != null && DestinationLatitude != null && DestinationLongitude != null)
+            {
+                Close();
+            }
+        }
+    }
+}
--- TaxiMeterUI/MainWindow.xaml.cs
+++ TaxiMeterUI/MainWindow.xaml.cs
@@ -19,8 +19,13 @@ namespace TaxiMeterUI
         public MainWindow()
         {
             InitializeComponent();
+            mainGrid.RowDefinitions[2].Height = new GridLength(0);
+            DateAndTime();
+        }
 
-            // Start a DispatcherTimer to update the TextBlock every second.
+        private void DateAndTime()
+        {
+            //Start a DispatcherTimer to update the TextBlock every second.
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += Timer_Tick;
@@ -35,17 +40,25 @@ namespace TaxiMeterUI
 
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
+            txtStatus.Text = "Status: Hired";
+            string startTime = DateTime.Now.ToString();
 
+            txtStartTime.Text = $"Start Time: {startTime}";
         }
 
         private void btnPayment_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void btnEnd_Click(object sender, RoutedEventArgs e)
         {
+            mainGrid.RowDefinitions[2].Height = new GridLength(200);
 
+            txtStatus.Text = "Status: For Hire";
+            string endTime = DateTime.Now.ToString();
+
+            txtEndTime.Text = $"End Time: {endTime}";
         }
 
         private void btnTariff_Click(object sender, RoutedEventArgs e)
@@ -61,6 +74,20 @@ namespace TaxiMeterUI
             else if ( txtTariff.Text == "Tariff 2")
             {
                 txtTariff.Text = "Tariff 3";
+            }
+        }
+
+        private void btnRideInfo_Click(object sender, RoutedEventArgs e)
+        {
+            Location popup = new Location();
+
+            if (popup.ShowDialog() == true)
+            {
+                int pickupLocationLat = popup.PickupLocationLatitude;
+                int pickupLocationLong = popup.PickupLocationLongitude;
+
+                int destinationLat = popup.DestinationLatitude;
+                int destinationLong = popup.DestinationLongitude;
             }
         }
     }
